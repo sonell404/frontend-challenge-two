ac0ce9a2fd98c2d7b76427a568746c7f1f9dfd10
sonell404 <sonell404@gmail.com>
2023-07-27 19:58:44 +0100

    Slider added - dots issue

--- main.js
+++ main.js
@@ -4,9 +4,25 @@ const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
-    ? navToggle.setAttribute("aria-expanded", "false") 
+    ? navToggle.setAttribute("aria-expanded", "false")
     : navToggle.setAttribute("aria-expanded", "true");
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
 });
-``
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  dots: true,
+  adaptiveHeight: false,
+  centerMode: true,
+  arrows: false,
+  responsive: [ 
+    {
+      breakpoint: 480, 
+      settings: { 
+        dots: false, 
+        arrows: true
+      }
+    }
+  ]
+});
+slider.init();
